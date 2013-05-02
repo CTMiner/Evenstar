c6004666d400c130bdc6b2f5e38817c44ef0978b
CTMiner <scienceguru1@gmail.com>
2013-05-02 19:57:30 -0400

    Added new pages & updated navbar
    New pages: Changelog and Approved Themes and Plugins

--- inc/navbar.php
+++ inc/navbar.php
@@ -1,6 +1,8 @@
 <table><tr>
-<td><a href="http://ctminer.koding.com/">Main Site</a></td>
 <td><a href="?page=home">Evenstar Home</a></td>
-<td><a href="http://ctminer.koding.com/wp/">News</a></td>
 <td><a href="?page=about">About Evenstar</a></td>
+<td><a href="?page=changelog">Changelog</a></td>
+<td><a href="?page=approved-themes-plugins">Approved Themes &amp; Plugins</a></td>
+<td><a href="http://ctminer.koding.com/">Main Site</a></td>
+<td><a href="http://ctminer.koding.com/wp/">Blog</a></td>
 </tr></table>
---
+++ pages/approved-themes-plugins.php
@@ -0,0 +1,8 @@
+<h2>Approved Themes</h2>
+<ul>
+    <li>Evenstar Navy (evenstar-navy) by CTMiner</li>
+</ul>
+<h2>Approved Plugins</h2>
+<ul>
+    <li>[WIP] Admin Dashboard (admin) by CTMiner</li>
+</ul>
