4303d4ac2348b4cc1b1411cf1e8b1ad45ebfdd27
CTMiner <scienceguru1@gmail.com>
2013-05-02 22:35:22 -0400

    New 'Sunset' theme

---
+++ themes/evenstar-sunset/index.php
@@ -0,0 +1,17 @@
+<?php
+require_once("themes/$theme/functions.php");
+runEvent("beforeLoad");
+require_once("themes/$theme/header.php");
+require 'inc/header.php';
+runEvent("header");
+echo("\n</div>\n<div class=\"navbar\">");
+require 'inc/navbar.php';
+echo("\n</div>\n<div class=\"content\">");
+runEvent("beforeContent");
+getContent();
+runEvent("afterContent");
+echo("</div>\n<div class=\"footer\">");
+require 'inc/footer.php';
+runEvent("footer");
+require_once "themes/$theme/footer.php";
+?>
