62ebefaccf12fa01132fa293edaae563a3d48e67
CTMiner <scienceguru1@gmail.com>
2013-08-16 20:53:27 -0400

    Update settings.php
    Added headerOnHomeOnly setting and added direct access safeguard

--- settings.php
+++ settings.php
@@ -1,4 +1,5 @@
 <?php
+if (realpath($_SERVER['SCRIPT_FILENAME']) == realpath(__FILE__)) { die('This page is not meant to be accessed directly.'); }
 // Set variables here
 $sitename = "Evenstar"; // Your site name here
 $motto = "Evenstar CMS"; // Your motto/slogan/tagline
@@ -8,4 +9,5 @@ $jsLoc = "header"; // Set to "header" or "footer" for where you want to load Jav
 $navbar = true; // Set to true/false - Enable/Disable navbar
 $defaultPage = "home"; // Page to load when no page specified
 $theme = "evenstar-navy"; // Set theme directory
+$headerOnHomeOnly = false; // Load header.php in the inc folder only on the home page
 ?>
