e8e7fbe21a43a6831eb81ece40e39fbc94241daa
CTMiner <scienceguru1@gmail.com>
2013-08-19 11:46:32 -0400

    Update actions.php
    Fixed mistake

--- themes/clean/actions.php
+++ themes/clean/actions.php
@@ -2,5 +2,5 @@
 newAction("beforeContent", "pageTitles");
 newAction("footerTxt", "themeCreds");
 function pageTitles() { global $pagename; echo("<h1>" . $pagename . "</h1>\n<hr />\n"); }
-function themeCreds() { echo("Clean is a CMS written by CTMiner - Theme is <i>Clean</i> by CTMiner"); }
+function themeCreds() { echo("Evenstar is a CMS written by CTMiner - Theme is <i>Clean</i> by CTMiner"); }
 ?>
